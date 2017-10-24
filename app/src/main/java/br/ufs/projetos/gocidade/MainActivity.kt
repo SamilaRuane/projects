b2f9d3de9d5941768c99c17d7ce01e6f1af308a0
Samila Ruane <samilaruane@gmail.com>
2017-10-23 23:11:10 -0300

    inserção do mapa na activity principal

--- app/src/main/java/br/ufs/projetos/gocidade/MainActivity.kt
+++ app/src/main/java/br/ufs/projetos/gocidade/MainActivity.kt
@@ -2,11 +2,46 @@ package br.ufs.projetos.gocidade
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mOrigem : LatLng
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        fragment.getMapAsync { callback ->
+                    callback.mapType = GoogleMap.MAP_TYPE_NORMAL
+                    mOrigem = LatLng(-23.561706, -46.655981)
+                    callback.animateCamera(CameraUpdateFactory.newLatLng(mOrigem))
+                    callback.addMarker(MarkerOptions()
+                            .position(mOrigem)
+                            .title("Av. Paulista")
+                            .snippet("São Paulo")
+                            .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_directions_car_24dp)))
+
+            val auxPlace = LatLng(-23.88888, -46.77777)
+
+            callback.addMarker(MarkerOptions()
+                    .position(auxPlace)
+                    .title("Av. Paulista")
+                    .snippet("São Paulo")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_pin_drop_24dp)))
+
+
+            callback.uiSettings.isMapToolbarEnabled = false
+            callback.uiSettings.isZoomControlsEnabled = true
+            callback.uiSettings.setAllGesturesEnabled(true)
+
+        }
     }
 }
